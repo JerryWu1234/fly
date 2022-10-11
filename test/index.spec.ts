f85f890ed05c2d8f8242300e42653610760a8958
wuls <linsheng.wu@beantechs.com>
2022-10-11 18:50:29 +0800

    feat: add u function

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { inspect } from '../src/inspect'
-import { Afn, run } from '../src/index'
+import { Afn, Ufn, run } from '../src/index'
 test('inspect', async () => {
   const cwdA = `${process.cwd()}/feature/a`
   const resultA = await inspect({ cwd: cwdA })
@@ -16,5 +16,15 @@ test('inspect', async () => {
 test('test a command', async () => {
   process.env.NODEDEV = 'debug'
   const resultA = await run(Afn, [], `${process.cwd()}/feature/a`)
-  expect(resultA, 'pnpm install')
+  expect(resultA).toBe('pnpm install')
+})
+
+test('test Afn', async () => {
+  const val = Afn('yarn', ['install', '-g'])
+  expect(val, 'yarn add install -g')
+})
+
+test('test Ufn', async () => {
+  const val = Ufn('yarn', ['dev'])
+  expect(val, 'yarn run dev')
 })

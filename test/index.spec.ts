3abfa9d2aa1b68fd9362872f5cb244e0265a2687
wuls <linsheng.wu@beantechs.com>
2022-10-13 14:49:41 +0800

    feat: record last command

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from 'vitest'
 import { inspect } from '../src/inspect'
 import { Afn, Ufn, run } from '../src/index'
+import { getPackageJson } from '../src/util'
+import { recordCommand } from '../src/command/u'
 test('inspect', async () => {
   const cwdA = `${process.cwd()}/feature/a`
   const resultA = await inspect({ cwd: cwdA })
@@ -27,4 +29,28 @@ test('test Afn', async () => {
 test('test Ufn', async () => {
   const val = Ufn('yarn', ['dev'])
   expect(val, 'yarn run dev')
+})
+
+test('test read package.json', async () => {
+  const cwdA = `${process.cwd()}/feature/a`
+  const jsonA = getPackageJson(cwdA)
+  expect(jsonA).toMatchSnapshot()
+  const cwdB = `${process.cwd()}/feature/a/pnpm-lock.yaml`
+  const jsonB = getPackageJson(cwdB)
+  expect(jsonB).toMatchSnapshot()
+})
+
+test('test merge object', () => {
+  const cwdA = `${process.cwd()}/feature/a`
+  const v = recordCommand('pnpm', ['dev', '--port:300'], cwdA, {})
+  expect(v).toMatchSnapshot()
+  const b = recordCommand('pnpm', ['dev', '--port:300'], cwdA, {
+    feature: 'pnpm run dev --port:300',
+    aa: 'pnpm run dev',
+  })
+  expect(b).toMatchSnapshot()
+  const c = recordCommand('pnpm', ['dev', '--port:300'], cwdA, {
+    aa: 'pnpm run dev',
+  })
+  expect(c).toMatchSnapshot()
 })
